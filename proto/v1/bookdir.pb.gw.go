4b1f25720d2334e5c113cb4f71b57ef318791c52
lhz <pepitsizous@zoho.com>
2021-05-04 18:39:03 +0300

    change one rest endpoint

--- proto/v1/bookdir.pb.gw.go
+++ proto/v1/bookdir.pb.gw.go
@@ -677,7 +677,7 @@ var (
 
 	pattern_BookDir_GetAllAuthors_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "v1", "authors"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_BookDir_GetBooksByAuthor_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v1", "authors", "AuthorID"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_BookDir_GetBooksByAuthor_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v1", "author", "AuthorID"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_BookDir_GetAllBooks_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "v1", "books"}, "", runtime.AssumeColonVerbOpt(true)))
 
